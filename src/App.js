e5800e0d0801ede96bc33cd0433eb411e3bca79f
Prince Slater <everythinginmoderation2@gmail.com>
2021-05-08 16:19:03 +0700

    deployed front end and backend

--- src/App.js
+++ src/App.js
@@ -4,9 +4,17 @@ import Header from "./Header";
 import Home from "./Home";
 import Checkout from "./Checkout";
 import Login from "./Login";
+import Payment from "./Payment";
+import Orders from "./Orders"
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { auth } from "./firebase";
 import { useStateValue } from "./StateProvider";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+
+const promise = loadStripe(
+  "pk_test_51II3ZULS0oTXkwrZviA9BmTS8sNLZaZSuCJGemvQElsObGpQrRnL6xUV2AjgWuRqmEiZ5PP5vdq7UF0DYxDyGZTl003KcvxvVh"
+);
 
 function App() {
   const [{}, dispatch] = useStateValue();
@@ -24,16 +32,22 @@ function App() {
           user: null,
         });
       }
-    })
+    });
   }, []);
-    
+
   return (
     <Router>
       <div className="app">
+        <Route path="/login" component={Login} />
         <Header />
         <Switch>
-          <Route path="/login" component={Login} />
           <Route path="/checkout" component={Checkout} />
+          <Route path="/payment">
+            <Elements stripe={promise}>
+              <Payment />
+            </Elements>{" "}
+          </Route>
+          <Route path="/orders" component={Orders}/>
           <Route exact path="/" component={Home} />
         </Switch>
       </div>
